b79fa25e9575876a965670d0807a6c926648ea42
Martin Kerschbaumer <maff32@gmx.com>
2018-09-13 15:49:11 +0200

    rename rearrange_signals* to rearrange_fits*

--- R/fits.R
+++ R/fits.R
@@ -1,33 +1,35 @@
-# signal-related functions
+# fit-related functions
 
 # rearrange signals-table according to fit-performance
 #' @importFrom dplyr arrange desc right_join select
 #' @importFrom magrittr "%>%"
 #' @importFrom mbte is_tbl_mbte mbte_reconstruct
-rearrange_signals <- function(signals, metrics) {
-  stopifnot(is_tbl_mbte(signals))
+rearrange_fits <- function(fits, metrics) {
+  stopifnot(is_tbl_mbte(fits))
 
   # only keep signal_id-column and result (join afterwards should only add
   # the result-column)
   metrics <- select(metrics, signal_id, result)
 
   # add result column and reorder rows
-  signals %>%
+  fits %>%
     right_join(metrics, by = "signal_id") %>%
     arrange(-desc(result)) %>%
     select(-result) %>%
-    mbte_reconstruct(signals)
+    mbte_reconstruct(fits)
 }
 
 # identical to rearrange_signals() - takes reactives as input (verified via
 # is.reactive())
-#' @importFrom shiny is.reactive req
-rearrange_signals_rv <- function(signals, metrics) {
-  stopifnot(is.reactive(signals), is.reactive(metrics))
+#' @importFrom shiny is.reactive reactive req
+rearrange_fits_rv <- function(fits, metrics) {
+  stopifnot(is.reactive(fits), is.reactive(metrics))
 
-  signals <- signals()
-  metrics <- metrics()
-  req(signals, metrics)
+  reactive({
+    fits <- fits()
+    metrics <- metrics()
+    req(fits, metrics)
 
-  rearrange_signals(signals, metrics)
+    rearrange_fits(fits, metrics)
+  })
 }
