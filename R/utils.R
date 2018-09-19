cb98f5bd2527243cfb29092df765f5b00081a8da
Martin Kerschbaumer <maff32@gmx.com>
2018-09-19 14:01:45 +0200

    add expr_input()

--- R/utils.R
+++ R/utils.R
@@ -12,3 +12,33 @@ r_extractor <- function(store, target) {
     store[[target]]
   })
 }
+
+# construct shiny.plugin for expression input (parse expression)
+#' @importFrom shiny NS textInput
+expr_input_ui <- function(id, ...) {
+  ns <- NS(id)
+
+  textInput(ns("expr_str"), ...)
+}
+
+#' @importFrom rlang parse_expr
+#' @importFrom shiny isTruthy need reactive throttle validate
+expr_input_server <- function(input, output, session, throttle_delay = 200,
+                              default_value = TRUE) {
+  expr <- reactive({
+    expr_str <- input$expr_str
+    if (isTruthy(expr_str)) {
+      tryCatch(parse_expr(expr_str), error = function(e) {
+        validate(need(FALSE, "error while parsing expression"))
+      })
+    } else {
+      # return default value / expression
+      default_value
+    }
+  })
+
+  throttle(expr, throttle_delay)
+
+  # return parsed expression
+  expr
+}
