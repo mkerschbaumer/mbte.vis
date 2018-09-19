71123dfd0df3e3e4aeaf57e270d92ae2aabde805
Martin Kerschbaumer <maff32@gmx.com>
2018-09-19 15:31:44 +0200

    in rearrange_fits(): use inner_join() instead of right_join() + add ellipsis argument to rearrange_fits_rv()

--- R/fits.R
+++ R/fits.R
@@ -1,7 +1,7 @@
 # fit-related functions
 
 # rearrange signals-table according to fit-performance
-#' @importFrom dplyr arrange desc right_join select
+#' @importFrom dplyr arrange desc inner_join select
 #' @importFrom magrittr "%>%"
 #' @importFrom mbte is_tbl_mbte mbte_reconstruct
 #' @importFrom rlang syms
@@ -15,7 +15,7 @@ rearrange_fits <- function(fits, metrics, by = "signal_id") {
 
   # add result column and reorder rows
   fits %>%
-    right_join(metrics, by = by) %>%
+    inner_join(metrics, by = by) %>%
     arrange(-desc(result)) %>%
     select(-result) %>%
     mbte_reconstruct(fits)
@@ -24,7 +24,7 @@ rearrange_fits <- function(fits, metrics, by = "signal_id") {
 # identical to rearrange_signals() - takes reactives as input (verified via
 # is.reactive())
 #' @importFrom shiny is.reactive reactive req
-rearrange_fits_rv <- function(fits, metrics) {
+rearrange_fits_rv <- function(fits, metrics, ...) {
   stopifnot(is.reactive(fits), is.reactive(metrics))
 
   reactive({
@@ -32,7 +32,7 @@ rearrange_fits_rv <- function(fits, metrics) {
     metrics <- metrics()
     req(fits, metrics)
 
-    rearrange_fits(fits, metrics)
+    rearrange_fits(fits, metrics, ...)
   })
 }
 
