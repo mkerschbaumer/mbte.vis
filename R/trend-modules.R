c9dcc1056b5c608f2abdf5695b0e26b63a3e77bb
Martin Kerschbaumer <maff32@gmx.com>
2019-02-10 15:10:24 +0100

    add tm_extract_store() export S3 methods for tm_extract_* functions

--- R/trend-modules.R
+++ R/trend-modules.R
@@ -4,6 +4,7 @@ tm_extract_quo <- function(x, ...) {
 }
 
 # implementation for default trend module
+#' @export
 tm_extract_quo.dtm <- function(x, ...) {
   x$fit_quo
 }
@@ -11,4 +12,20 @@ tm_extract_quo.dtm <- function(x, ...) {
 #' @importFrom purrr map
 tm_extract_quos <- function(l) {
   map(l, tm_extract_quo)
+}
+
+tm_extract_store <- function(x, ...) {
+  UseMethod("tm_extract_store")
+}
+
+# implementation for default trend module
+#' @export
+tm_extract_store.dtm <- function(x, ...) {
+  x$store
+}
+
+# extract coefficient stores form trend module
+#' @importFrom purrr map
+tm_extract_stores <- function(l) {
+  map(l, tm_extract_store)
 }
